87edf356496827399ebf6adc228ce04666f2fe39
Arik Karim <81914628+ArikKarim@users.noreply.github.com>
2025-01-21 15:00:34 -0500

    Update cylinder_area.py

--- cylinder_area.py
+++ cylinder_area.py
@@ -1,7 +1,7 @@
 # this program calculates the surface area of a cylinder
 
-# ask the user for the inputs
+# step 1: ask the user for the inputs
 
-# do the math calc to calc the sa of a cylinder
+# step 2: do the math calc to calc the sa of a cylinder
 
-# print the output to the user
+# step 3: print the output to the user
