582e39bd9258626350722a4fe56ff76b87025623
Arik Karim <81914628+ArikKarim@users.noreply.github.com>
2025-01-30 15:17:34 -0500

    Update cylinder_area.py

--- cylinder_area.py
+++ cylinder_area.py
@@ -1,7 +1,28 @@
-# this program calculates the surface area of a cylinder
+import math
 
-# step 1: ask the user for the inputs
+def float_input(prompt):
+  while True:
+    try:
+      x = float(input(prompt))
+      # if we get to this line, then we are good
+      if x <= 0:
+        print('Cannot enter negative number or zero - try again.')
+      else:
+        return x
+    except ValueError:
+      print('Invalid value for float - try again.')
 
-# step 2: do the math calc to calc the sa of a cylinder
+# ask the user for the radius
+radius = float_input('Enter the radius of the cylinder: ')
+print('The radius is', radius)
 
-# step 3: print the output to the user
+# ask the user for the height
+height = float_input('Enter the height of the cylinder: ')
+print('The height is', height)
+
+# do the math calc to calc the sa of a cylinder
+sa = ((2 * math.pi * radius * height) + (2 * math.pi * (radius ** 2)))
+
+# print the output to the user
+print('The surface area is', sa) # raw form
+print('The surface area is {:.2f}'.format(sa))
