2081f9c43a24166dd6c6e5263d6be8abcd5ae108
Arik Karim <81914628+ArikKarim@users.noreply.github.com>
2025-02-06 15:17:53 -0500

    Update letter_grade.py

--- letter_grade.py
+++ letter_grade.py
@@ -1,31 +1,32 @@
 # validation function
-def float_input(question):
+def float_input(question, low, high):
   while True:
     try:
       x = float(input(question))
-      if x < 0:
-        print('Cannot enter negative number - try again.')
+      if x < low or x > high:
+        print('Cannot enter num <', low, 'or >', high, '- try again.')
       else:
         return x
     except ValueError:
       print('Invalid value for float - try again.')
 
-# call the validation function
-score = float_input('Enter a score: ')
-print('You entered a score of', score)
+# ask user for input and provide output
+while True:
+  score = float_input('Enter a score: ', 0, 100)
+  print('You entered a score of', score)
+  if score >= 90:
+    letter_grade = 'A'
+  elif score >= 80:
+    letter_grade = 'B'
+  elif score >= 70:
+    letter_grade = 'C'
+  elif score >= 60:
+    letter_grade = 'D'
+  else:
+    letter_grade = 'F'
+  print('Your letter grade is', letter_grade)
 
-# determine the letter grade - if statement
-if score >= 90:
-  letter_grade = 'A'
-elif score >= 80:
-  letter_grade = 'B'
-elif score >= 70:
-  letter_grade = 'C'
-elif score >= 60:
-  letter_grade = 'D'
-else:
-  letter_grade = 'F'
-
-# print the letter grade
-print('Your letter grade is', letter_grade)
-
+  # loop 
+  again = input('Do you wish to loop again? Y/N: ').upper()
+  if again == 'N':
+    break  
