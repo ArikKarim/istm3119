b85e8e33911ae61524dcb9f3ef3f0723c5992c52
Arik Karim <81914628+ArikKarim@users.noreply.github.com>
2025-02-18 13:28:51 -0500

    Create tools.py

---
+++ tools.py
@@ -0,0 +1,28 @@
+import math
+import random
+
+
+def float_validate_input(prompt, low, high):
+   while True:
+       user_input = input(prompt)
+       try:
+           value = float(user_input)
+           if value < low or value > high:
+               print("Error. Number is outside the given range (",low, " - ",high, "). Please try again")
+           else:
+               return value
+       except ValueError:
+           print('Error. Please enter a number')
+
+
+def int_validate_input(prompt, low, high):
+   while True:
+       unput = input(prompt)
+       try:
+           value = int(unput)
+           if value <= low and value >= high:
+               print("Error. Number is outside the given range (",low, " - ",high, "). Please try again")
+           else:
+               return value
+       except ValueError:
+           print('Error. Please enter a number')
