cd28b4d798d5d4df54f555910800183a191cab5a
Arik Karim <81914628+ArikKarim@users.noreply.github.com>
2025-03-20 14:40:52 -0400

    Update rock_paper_scissor.py

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -3,3 +3,22 @@
 import random
 
 # 3 strings for the 3 outcomes
+tie = 'It is a tie'
+won = 'Congrats, you won'
+lost = 'You lost'
+
+# ask the user for their choice
+prompt = 'Enter 1 for Rock, 2 for Paper, or 3 for Scissors: '
+# call the ask_user_for_int(prompt, 1, 3)
+
+# if statement to decide what word to print
+# based on what the user chose
+if user == 1:
+  print('You chose rock')
+elif user == 2: 
+  print('You chose paper')
+else:
+  print('You chose scissors')
+
+# 'ask' the computer what they choose = randint
+comp = random.randint(1, 3)
