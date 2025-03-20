c030f1fbd9fc857fee2accd6614eddfb68bc3d7e
Arik Karim <81914628+ArikKarim@users.noreply.github.com>
2025-03-20 15:18:30 -0400

    Update rps_with_tuple.py

--- rps_with_tuple.py
+++ rps_with_tuple.py
@@ -1,1 +1,20 @@
+import random
 
+# 3 strings for the 3 outcomes
+tie = 'It is a tie'
+won = 'Congrats, you won'
+lost = 'You lost'
+
+# tuples that are helpful for our code
+choices = ('rock, paper, scissors')
+
+rules = ((tie, lost, won),
+        (won, tie, lost),
+        (lost, won, tie))
+
+# ask the user for their choice but instead of using an if statement to print the word
+print('You chose', choices[user - 1])
+
+# for the comp choicem, do the same thing
+
+# determine the outcome from the game 
