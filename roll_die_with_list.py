7168974b43183860d5a9d61a202f05a942eb5b70
Arik Karim <81914628+ArikKarim@users.noreply.github.com>
2025-03-25 01:42:40 -0400

    Update roll_die_with_list.py

--- roll_die_with_list.py
+++ roll_die_with_list.py
@@ -1,26 +1,24 @@
-import math
 import random
-import tools
 
 def roll_many_and_keep_count():
-    
     # create a list of 6 int, set each to 0
     counters = [0, 0, 0, 0, 0, 0]
-    
-    for x in range(6000):
-        result = random.randint(1,6)
-        
-    # add one to the appropriate counter in our list
-    counters[result - 1] += 1
 
-    # print the report
+    # roll the die 6000 times and count the occurrences
+    for x in range(6000):
+        result = random.randint(1, 6)
+        # Add one to the appropriate counter in our list
+        counters[result - 1] += 1
+
+    print('Here is a report of how many times each side was rolled')
+
+    # use a for loop to print from the list
+    # loop 6 times (for each die side)
     for index in range(6):
-      # print the two messages
-      print('Side', index + 1, 'was rolled', counters[index], 'times')
-      print('Side', index + 1, 'was rolled', counters[index] / 6000, '% of the time')
+        print('Side', index + 1, 'was rolled', counters[index], 'times')
+        print('Side', index + 1, 'was rolled', counters[index] / 6000 * 100, '% of the time')
 
     print()
-    print('Here is a report of how many times each side was rolled')
 
 def main():
     roll_many_and_keep_count()
