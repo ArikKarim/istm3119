ca13b8e29032783633c91030c9326eab951c7605
Arik Karim <81914628+ArikKarim@users.noreply.github.com>
2025-03-25 15:19:46 -0400

    Update nato.py

--- nato.py
+++ nato.py
@@ -2,7 +2,10 @@ nato_map # copied over
 
 user_codes = input("Type in the letters you wish to speak").lower
 
-# user_codes "abc9"
+# user codes 'abc9'
+
+sentences = ""
 
 for c in user_codes:
-  print(c)
+  word = nato_map.get(c)
+  sentence = sentence + word + ""
