28fe7b4c2887e6b0bfdadf7c1f44cf8f5586d16c
Arik Karim <81914628+ArikKarim@users.noreply.github.com>
2025-03-27 15:40:12 -0400

    Update rpsls.py

--- rpsls.py
+++ rpsls.py
@@ -1,1 +1,22 @@
+import random
 
+# 3 strings for the 3 outcomes
+tie = 'It is a tie'
+won = 'Congrats, you won'
+lost = 'You lost'
+
+# tuples that are helpful for our code
+choices = ('rock', 'paper', 'scissors', 'lizard', 'spock')
+
+rules = ((tie, lost, won, ),
+        (won, tie, lost, ),
+        (lost, won, tie, ),
+        (
+
+# ask the user for their choice but instead of using an if statement to print the word
+print('You chose', choices[user - 1])
+
+# for the comp choice, do the same thing
+
+# determine the outcome from the game 
+print()
