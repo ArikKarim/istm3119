a8e031be08ba91234ab0073e1f15132c1eb3e96e
Arik Karim <81914628+ArikKarim@users.noreply.github.com>
2025-03-27 15:00:35 -0400

    Update nato.py

--- nato.py
+++ nato.py
@@ -1,41 +1,4 @@
-nato_map = {
-    'a': 'alpha',
-    'b': 'bravo',
-    'c': 'charlie', 
-    'd': 'delta',
-    'e': 'echo',
-    'f': 'foxtrot',
-    'g': 'golf',
-    'h': 'hotel',
-    'i': 'india',
-    'j': 'juliet',
-    'k': 'kilo',
-    'l': 'lima',
-    'm': 'mike',
-    'n': 'november',
-    'o': 'oscar',
-    'p': 'papa',
-    'q': 'quebec',
-    'r': 'romeo',
-    's': 'sierra',
-    't': 'tango',
-    'u': 'uniform',
-    'v': 'victor',
-    'w': 'whiskey',
-    'x': 'x-ray',
-    'y': 'yankee',
-    'z': 'zulu',
-    '0': 'zero',
-    '1': 'one',
-    '2': 'two',
-    '3': 'three',
-    '4': 'four',
-    '5': 'five',
-    '6': 'six',
-    '7': 'seven',
-    '8': 'eight',
-    '9': 'niner'
-}
+nato_map = None
 
 user_codes = input("Type in the code you wish to speak: ").lower()
 
