135e03f7aa4016a790d055a6923e12017e493755
Arik Karim <81914628+ArikKarim@users.noreply.github.com>
2025-03-27 14:38:00 -0400

    Update nato.py

--- nato.py
+++ nato.py
@@ -1,11 +1,37 @@
-nato_map # copied over
+nato_map = {
+    'a': 'alpha',
+    'b': 'bravo',
+    'c': 'charlie', 
+    'd': 'delta',
+    'e': 'echo',
+    'f': 'foxtrot',
+    'g': 'golf',
+    'h': 'hotel',
+    'i': 'india',
+    'j': 'juliet',
+    'k': 'kilo',
+    'l': 'lima',
+    'm': 'mike',
+    'n': 'november',
+    'o': 'oscar',
+    'p': 'papa',
+    'q': 'quebec',
+    'r': 'romeo',
+    's': 'sierra',
+    't': 'tango',
+    'u': 'uniform',
+    'v': 'victor',
+    'w': 'whiskey',
+    'x': 'x-ray',
+    'y': 'yankee',
+    'z': 'zulu'
+}
 
-user_codes = input("Type in the letters you wish to speak").lower
+user_codes = input("Type in the letters you wish to speak").lower()
 
 # user codes 'abc9'
 
 sentences = ""
-
 for c in user_codes:
   word = nato_map.get(c)
   if word is None:
